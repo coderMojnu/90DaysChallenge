224caffe74733c400e97e574da6c8753c8ce7e87
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-09-07 08:32:22 +0600

    shift removes and return 1st item

--- Day5/arrayAndFunction-day5/push.js
+++ Day5/arrayAndFunction-day5/push.js
@@ -1,3 +1,4 @@
+//push add new item to end of the array
 const brother = ["Nurul", "Milon", "Nasir", "Nannu", "Mofijul"];
 brother.push("Ashraful");
-console.log(brother);
+console.log(brother); //output: ['Nurul', 'Milon', 'Nasir', 'Nannu', 'Mofijul', 'Ashraful']
---
+++ Day5/arrayAndFunction-day5/shift.js
@@ -0,0 +1,5 @@
+//shift removes and return the first item
+const familyMember = ["me-Mojnu", "Monju", "Shahadat Hossain", "Mominul Islam", "Sajeda Khatun"];
+const firstItem = familyMember.shift();
+console.log(firstItem) //return "me-Mojnu"
+console.log(familyMember) //return  
