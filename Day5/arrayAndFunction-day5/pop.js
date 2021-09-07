1387ffc502de2c453bd874b6356afd9e08c9cd02
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-09-07 07:20:29 +0600

    startCode

---
+++ Day5/arrayAndFunction-day5/pop.js
@@ -0,0 +1,4 @@
+//pop removes and return the last element of the array
+const friendName = ["Morshed", "Ariful", "Asaduzzaman", "Edrish"];
+const deleteAFriend= friendName.pop();
+console.log(deleteAFriend);
