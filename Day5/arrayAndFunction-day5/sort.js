fc75a35a3fdf121273b78d0538309d51779f4799
Md. Mojnu Miah <mojnu.cse.pust@gmail.com>
2021-09-07 12:32:43 +0600

    sort retrun the new array by alphabetical order

---
+++ Day5/arrayAndFunction-day5/sort.js
@@ -0,0 +1,4 @@
+//sort return the new array by alphabetical order
+const studentName = ["Sarmin", "Bithy", "Shikha", "Jui", "Merina", "Sanjida"];
+const sortStudent = studentName.sort();
+console.log(studentName); //['Bithy', 'Jui', 'Merina', 'Sanjida', 'Sarmin', 'Shikha']
